57903d6a73a857ffdccb6a85d6dcb1edb5dd9e14
Mikita Kukavenka <nkukovenko@by.byelex.com>
2018-12-03 18:07:37 +0300

    Initial configuration of app

---
+++ app/app.module.js
@@ -0,0 +1,14 @@
+'use strict';
+// import './src/css/app.css';
+
+const initiativeApp = angular.module('initiativeApp', [
+]);
+
+
+import AppComponent from './src/app.component.js';
+
+initiativeApp.component('app', AppComponent);
+
+// initiativeApp.config([]);
+
+// initiativeApp.run([]);

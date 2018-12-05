b69324c2094ae73721b096465f5c8c98d7803479
Mikita Kukavenka <nkukovenko@by.byelex.com>
2018-12-05 18:47:16 +0300

    Changes in design

--- app/src/components/pc.component.js
+++ app/src/components/pc.component.js
@@ -11,6 +11,30 @@ function PCController($log) {
     ctrl.pc.actions[number] = !ctrl.pc.actions[number];
   }
 
+  ctrl.changeName = function() {
+    ctrl.pc.name = ctrl.name;
+    ctrl.editName();
+  }
+
+  ctrl.editName = function() {
+    if(!ctrl.editNameMode){
+      ctrl.name = ctrl.pc.name;
+    }
+    ctrl.editNameMode = !ctrl.editNameMode;
+  }
+
+  ctrl.changeHp = function() {
+    ctrl.pc.hp = ctrl.hp;
+    ctrl.editHp();
+  }
+
+  ctrl.editHp = function() {
+    if(!ctrl.editHpMode){
+      ctrl.hp = ctrl.pc.hp;
+    }
+    ctrl.editHpMode = !ctrl.editHpMode;
+  }
+
 }
 
 const PCComponent = {

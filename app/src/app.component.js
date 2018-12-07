91f7afcf4f1a92182c32debf468096c6951455f5
Mikita Kukavenka <nkukovenko@by.byelex.com>
2018-12-07 19:42:36 +0300

    Added saving to localstorage

--- app/src/app.component.js
+++ app/src/app.component.js
@@ -1,11 +1,20 @@
 "use strict";
 
-function AppController($log, focusService) {
+function AppController($log, $scope, $window, focusService) {
   const ctrl = this;
+  const localStorage = $window['localStorage'];
+  const STARAGE_PCS = 'PCS';
 
   ctrl.$onInit = function() {
     $log.debug("AppController init");
-    ctrl.pcs = [];
+    let pcs;
+    try {
+      pcs = JSON.parse(localStorage.getItem(STARAGE_PCS));
+    } catch (err) {
+      $log.error(err);
+    }
+    ctrl.pcs = (!pcs || !pcs.length) ? [] : pcs;
+    $log.debug("AppController ctrl.pcs", ctrl.pcs, pcs);
     ctrl.initiativeCount = 1;
   }
 
@@ -24,12 +33,19 @@ function AppController($log, focusService) {
   ctrl.clear = function() {
     ctrl.pcs = [];
   }
+
+  $scope.$watch('$ctrl.pcs', function(newVal, oldVal) {
+    console.log('AppController changed', newVal, oldVal);
+    localStorage.setItem(STARAGE_PCS, JSON.stringify(newVal));
+  }, true);
 }
 
 const AppComponent = {
   template: require('./app.html'),
   controller: [
     '$log',
+    '$scope',
+    '$window',
     'focusService',
     AppController
   ]

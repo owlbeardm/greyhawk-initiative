f5b50ea45a272b84178f2928ad5a1f7255e62aad
Mikita Kukavenka <nkukovenko@by.byelex.com>
2019-12-13 17:51:18 +0300

    added new styles

--- app/src/components/components.module.js
+++ app/src/components/components.module.js
@@ -4,10 +4,14 @@ const appComponents = angular.module('app.components', []);
 
 import PCComponent from './pc.component.js';
 import AddPCComponent from './addpc.component.js';
-import PageHeaderComponent from './pageheader.component.js';
 import ConditionComponent from './condition.component.js';
 
 appComponents.component('pc', PCComponent);
 appComponents.component('addpc', AddPCComponent);
-appComponents.component('pageheader', PageHeaderComponent);
 appComponents.component('condition', ConditionComponent);
+
+import PageFooterComponent from './page-footer/page-footer.component.js';
+appComponents.component('pageFooter', PageFooterComponent);
+
+import PageHeaderComponent from './page-header/page-header.component.js';
+appComponents.component('pageHeader', PageHeaderComponent);
